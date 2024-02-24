93b3206d0a02e3b6424857c53e1d5e40ce5719c0
eat-sleep-code <info@eat-sleep-code.com>
2024-02-23 23:04:54 -0700

    Fix an import

--- camera.py
+++ camera.py
@@ -1,6 +1,6 @@
 from libcamera import controls
 from functions import Echo, Console
-from globals import Cameras
+from globals import Cameras, Primary, Secondary
 from controls.touchscreen import Touchscreen
 
 
@@ -12,14 +12,14 @@ echo = Echo()
 touchscreen = Touchscreen()
 
 try:
-	Cameras.Primary.module.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+	Primary.module.set_controls({"AfMode": controls.AfModeEnum.Continuous})
 except Exception as ex:
 	console.info('Camera 1 does not support autofocus.')
 	pass
 
 if Cameras.count > 1:
 	try:
-		Cameras.Secondary.module.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+		Secondary.module.set_controls({"AfMode": controls.AfModeEnum.Continuous})
 	except Exception as ex:
 		console.info('Camera 2 does not support autofocus.')
 		pass
