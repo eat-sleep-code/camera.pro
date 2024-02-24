824ae2496a6888d01c8e4f82cf89193872e9da11
eat-sleep-code <info@eat-sleep-code.com>
2024-02-24 11:00:05 -0700

    Fix indentation

--- controls/remote.py
+++ controls/remote.py
@@ -7,17 +7,17 @@ console = Console()
 actions = Actions()
 
 class Remote:
-    device = evdev.InputDevice('/dev/input/event1')
-    console.debug(device)
+	device = evdev.InputDevice('/dev/input/event1')
+	console.debug(device)
 
-    try:
-        for event in device.read_loop():
-            if event.type == evdev.ecodes.EV_KEY:
-                if event.value == 1:  # Short Press
-                    actions.CaptureImage()
-                elif event.value == 2:  # Long Press
-                    actions.CaptureVideo()
-                elif event.value == 0:  # Release
-                    pass
-    except KeyboardInterrupt:
-        pass
+	try:
+		for event in device.read_loop():
+			if event.type == evdev.ecodes.EV_KEY:
+				if event.value == 1:  # Short Press
+					actions.CaptureImage()
+				elif event.value == 2:  # Long Press
+					actions.CaptureVideo()
+				elif event.value == 0:  # Release
+					pass
+	except KeyboardInterrupt:
+		pass
